b5393cc9b07bc5098551ef0c63c8ad2fd1aec690
max-pn <maximilian.partin@icloud.com>
2026-03-19 21:18:18 +0100

    hypervisor auto-resizing fix

--- hosts/vm-aarch64.nix
+++ hosts/vm-aarch64.nix
@@ -25,4 +25,12 @@
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
+
+  # apply hypervisor auto-resizing fix
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "xrandr-auto" ''
+      xrandr --output Virtual-1 --auto
+    '')
+
+  ];
 }
