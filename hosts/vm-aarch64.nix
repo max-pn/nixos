6822ab25f2387b73396edf6320349d3570319572
max-pn <maximilian.partin@icloud.com>
2026-03-25 16:51:32 +0100

    missing package for auto-resize fix

--- hosts/vm-aarch64.nix
+++ hosts/vm-aarch64.nix
@@ -30,6 +30,8 @@
 
   # apply hypervisor auto-resizing fix
   environment.systemPackages = with pkgs; [
+    xrandr
+
     (writeShellScriptBin "xrandr-auto" ''
       xrandr --output Virtual-1 --auto
     '')
